d9506753e72343d855291e180b64fdfaf0d7cc40
aniketbonagir <>
2020-02-25 18:15:21 +0530

    Update maxSumIncreasingSubsequence.js

--- Machine Coding JS Questions/maxSumIncreasingSubsequence.js
+++ Machine Coding JS Questions/maxSumIncreasingSubsequence.js
@@ -6,6 +6,10 @@ generated from a strictly-increasing subsequence in the array. The second elemen
 of the numbers in that subsequence. A subsequence is dened as a set of numbers that are not
 necessarily adjacent but that are in the same order as they appear in the array. Assume that there will
 only be one increasing subsequence with the greatest sum
+
+Sample input: [10, 70, 20, 30, 50, 11, 30]
+Sample output: [110, [10, 20, 30, 50]]
+
 */
 
 function maxSumIncreasingSubsequence(array) {
